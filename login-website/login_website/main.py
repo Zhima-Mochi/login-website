c58d58aaea50d5bb5f10ca1d171cfe822f831964
Zhima-Mochi <yehgoter@gmail.com>
2022-03-07 17:57:46 +0800

    architecture router

--- login-website/login_website/main.py
+++ login-website/login_website/main.py
@@ -1,10 +1,7 @@
-from asyncio.log import logger
-from datetime import datetime
-from fastapi import Depends, FastAPI, HTTPException, status, Response
-from . import models, schemas, database, crud, authentication_tool, password_tool, dependents
-from starlette import status
+from fastapi import FastAPI
+from login_website import schemas, database
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from login_website.routers.auth import router as auth_router
 
 
 app = FastAPI()
@@ -22,6 +19,9 @@ app.add_middleware(
 )
 
 
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+
+
 @app.on_event("startup")
 async def startup():
     # schemas.Base.metadata.drop_all(bind=database.sync_engine)
@@ -32,38 +32,3 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.database.disconnect()
-
-
-@app.post("/register", status_code=status.HTTP_201_CREATED, response_model=models.User)
-async def register(user_create: models.UserCreate, connection=Depends(database.get_connection)) -> models.User:
-    async with connection.transaction():
-        try:
-            user_db = await crud.create_user(connection, user_create)
-        except Exception as e:
-            logger.error(e)
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
-        return user_db
-
-
-@app.post("/token")
-async def create_token(response: Response, form_data: OAuth2PasswordRequestForm = Depends(OAuth2PasswordRequestForm), connection=Depends(database.get_connection), redis_conn=Depends(database.get_redis_conn)):
-    async with connection.transaction():
-        user_email = form_data.username
-        user_password = form_data.password
-        user = await authentication_tool.authenticate_user(connection, user_email, user_password)
-        if user is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-        token, expire_time = password_tool.create_access_token(data={
-                                                               "sub": user_email})
-        await redis_conn.set(user_email, token, (expire_time-datetime.now()).seconds)
-        response.set_cookie(key="session", value=token, expires=expire_time.ctime(),
-                            httponly=True, samesite="lax")
-        return token
-
-
-@app.get("/login_status")
-async def login_status(response: Response, login_status: str = Depends(dependents.get_login_status)):
-    if login_status != models.LoginStatus.auth:
-        response.delete_cookie(key="session")
-    return login_status
---
+++ login-website/login_website/routers/auth.py
@@ -0,0 +1,44 @@
+
+from login_website import crud, database, password_tool, dependents, models, authentication_tool
+from asyncio.log import logger
+from datetime import datetime
+from fastapi import Depends, APIRouter, HTTPException, status, Response
+from starlette import status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+router = APIRouter()
+
+
+@router.post("/register", status_code=status.HTTP_201_CREATED, response_model=models.User)
+async def register(user_create: models.UserCreate, connection=Depends(database.get_connection)) -> models.User:
+    async with connection.transaction():
+        try:
+            user_db = await crud.create_user(connection, user_create)
+        except Exception as e:
+            logger.error(e)
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
+        return user_db
+
+
+@router.post("/token")
+async def create_token(response: Response, form_data: OAuth2PasswordRequestForm = Depends(OAuth2PasswordRequestForm), connection=Depends(database.get_connection), redis_conn=Depends(database.get_redis_conn)):
+    async with connection.transaction():
+        user_email = form_data.username
+        user_password = form_data.password
+        user = await authentication_tool.authenticate_user(connection, user_email, user_password)
+        if user is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        token, expire_time = password_tool.create_access_token(data={
+                                                               "sub": user_email})
+        await redis_conn.set(user_email, token, (expire_time-datetime.now()).seconds)
+        response.set_cookie(key="session", value=token, expires=expire_time.ctime(),
+                            httponly=True, samesite="lax")
+        return token
+
+
+@router.get("/login_status")
+async def login_status(response: Response, login_status: str = Depends(dependents.get_login_status)):
+    if login_status != models.LoginStatus.auth:
+        response.delete_cookie(key="session")
+    return login_status
