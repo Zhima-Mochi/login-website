ead9d1fc1a35494623a0e11cc724530f553a041f
Zhima-Mochi <yehgoter@gmail.com>
2022-03-11 10:10:36 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# LOGIN WEBSITE
+
+## Overview
+This application offer function of user authentication, when users register and login, they can edit their information until they logout or json web token is expired.
+
+## Technology Stack
+### Infrastructure
+* MySQL
+* Redis
+* Nginx
+* ASGI
+* Docker
+* Json Web Tokens (JWT) Authentication
+* CSRF protection
+
+### Backend RESTful API
+* FastAPI
+* SQLAlchemy ORM
+* Databases Asynchronous I/O
+
+### Frontend Webpage
+* Creat React App
+* Redux
+* Tailwind CSS
+
+## Getting started
+```sh
+docker-compose up
+```
+
--- nginx-proxy/frontend-login-website/src/pages/MainPage.jsx
+++ nginx-proxy/frontend-login-website/src/pages/MainPage.jsx
@@ -5,7 +5,12 @@ import Header from "../components/Header";
 function Message() {
     const userName = useSelector(state => state.userNameReducer);
     if (userName !== "") {
-        return <div className="mb-4">Welcome to Main Page!</div>;
+        return (
+            <div className="flex flex-col text-center">
+                <div className="mb-4">Welcome to Main Page!</div>
+                <div className="mb-4">You can check or edit your <NavLink to='/user' className="text-blue-500 underline">information</NavLink> now!</div>
+            </div>
+        );
     } else {
         return <div className="mb-4">Please <NavLink to='/login' className="text-blue-500 underline">login</NavLink> or <NavLink to='/register' className="text-red-300 underline">register</NavLink> first.</div>;
     }
